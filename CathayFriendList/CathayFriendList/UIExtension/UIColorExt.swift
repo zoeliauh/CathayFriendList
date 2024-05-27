80ce056c6f1dcb347d7b588a5afbccdb384aafa8
zoeliauh <ibm801227@hotmail.com>
2024-05-27 11:11:09 +0800

    fix: UIColorExt file missing

---
+++ CathayFriendList/CathayFriendList/UIExtension/UIColorExt.swift
@@ -0,0 +1,32 @@
+//
+//  UIColorExt.swift
+//  CathayFriendList
+//
+//  Created by woanjwu liauh on 2024/5/27.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let appBackgroundColor: UIColor = .white
+    
+    static let headerViewBackgroundColor: UIColor = UIColor(red: 252/255, green: 252/255, blue: 252/255, alpha: 1)
+    
+    static let mainLabelColor: UIColor = UIColor(red: 71/255, green: 71/255, blue: 71/255, alpha: 1)
+    
+    static let subLabelColor: UIColor = UIColor(red: 152/255, green: 152/255, blue: 152/255, alpha: 1)
+
+    static let selectedItemBottomLineColor: UIColor = UIColor(red: 236/255, green: 0/255, blue: 140/255, alpha: 1)
+
+    static let searchBarColor: UIColor = UIColor(red: 142/255, green: 142/255, blue: 147/255, alpha: 1)
+    
+    static let separateLineColor: UIColor = UIColor(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
+    
+    static let friendCellSeparateLineColor: UIColor = UIColor(red: 228/255, green: 228/255, blue: 228/255, alpha: 1)
+    
+    static let addFriendButtonColor: UIColor = UIColor(red: 166/255, green: 204/255, blue: 66/255, alpha: 1)
+    
+    
+}
+ 
