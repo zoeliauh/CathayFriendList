569b246c0e69c6cfc02379a0d9f75d5864027063
zoeliauh <ibm801227@hotmail.com>
2024-05-27 10:32:11 +0800

    prepare UIExt and Components

---
+++ CathayFriendList/CathayFriendList/UIExtension/UIFontExt.swift
@@ -0,0 +1,22 @@
+//
+//  UIFontExt.swift
+//  CathayFriendList
+//
+//  Created by woanjwu liauh on 2024/5/26.
+//
+
+
+import UIKit
+
+extension UIFont {
+    static let userNameFont: UIFont = .systemFont(ofSize: 17, weight: .medium)
+    
+    static let userIdFont: UIFont = .systemFont(ofSize: 17, weight: .regular)
+    
+    static let friendNameFont: UIFont = .systemFont(ofSize: 16, weight: .regular)
+    
+    static let selectedItemFont: UIFont = .systemFont(ofSize: 13, weight: .medium)
+    
+    static let notSelectedItemFont: UIFont = .systemFont(ofSize: 13, weight: .regular)
+    
+}
